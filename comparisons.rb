08a63951c2d1f1de65f3c0efd7b1bbddec50af48
Clyde Autin <clyde.autin@gmail.com>
2024-02-18 16:41:44 -0800

    completed comparisons

--- comparisons.rb
+++ comparisons.rb
@@ -24,24 +24,37 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
 # this should print: true
 
+puts "Q: Are there less teachers than students?", number_teachers < number_students
+
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
+
+puts "is number of teachers equal to string of teachers?", number_teachers == string_teachers
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
 
+puts "Is number fo teachers not equal to number of students?", number_teachers != number_students
+
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
+
+puts "Is number of students greater than or equal to 20?", number_students >= 20
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
 # this should print: false
 
+puts number_students >= 21
+
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
+
+puts number_students <= 20
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
 
+puts number_students <= 21
 
 #-------------------
 # PART 2: Articulating what you are doing
@@ -54,21 +67,24 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
+# This will print out the result of the comparison boolean is 4 less than 9. Result will be true
 
 books = 3
 puts 4 < books
 # YOU DO: Explain.
+# Books is established as an argument and has been given a value of 3. a line will be printed where a comparion of 4 is less than the value of books (3) will take place. Result will be false
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
+# A new argument, friends is given a value of 6 while sibling is given a value of 2. then a line will be printed to see if value of friends is greater than value of siblings. Result will be true
 
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
-
+# new argument attendees is given a value of 9 while a second argument meals is given a value of 8. a line will be printerd to see if value of attendees is not equal to value of meals. Result will be true
 
 #-------------------
 # PART 3: Logical Operators
@@ -91,14 +107,19 @@ age = 1
 # YOU DO:
 # Determine if the dog loves to play and loves treats
 
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
 
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
 
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
 
+puts loves_to_play && age <= 1
+
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: The final answer came out to "true" because the value of "age <= 1" is "true"
